ff89b9342bf97e01c74095a7e8efc5c75ecdfb8a
Mohsen Iranmanesh <mhsn.iranmanesh@gmail.com>
2017-09-09 20:41:25 +0430

    git repository initiated

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  devServer:{
+    inline: true,
+    contentBase: './build',
+    port: 9000
+  },
+  module:{
+    rules: [
+      {
+        test:  /\.js$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+      }
+    ]
+  },
+};

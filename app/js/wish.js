4c704fc7a83b0e1446cf700562853d6d33a94c77
Amirhosein Shirani <amirhhh.shirani@yahoo.com>
2017-09-10 21:03:58 +0430

    SignUp/In JS without AJAX

--- app/js/wish.js
+++ app/js/wish.js
@@ -1,15 +1,129 @@
+var freelancer;
+var client ;
+var password = $('#signupPassInput');
+var username = $('#signupUsernameInput');
+var mobileNumber = $('#mobilePassInput');
+var readyFunc = function(client , freelancer){
+    $("#btn_sign_up_freelancer").click(function(){
+      freelancer++;
+		window.location.href = "signup-user-pass.html?freelancer=" +freelancer;
+	
+		
+	});
+};
 
-$(document).ready(function() {
-    var persianNumbers = ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹'];
-    $('.persian').text(function(i, v) {
-        var chars = v.split('');
-        for (var i = 0; i < chars.length; i++) {
-            if (/\d/.test(chars[i])) {
-                chars[i] = persianNumbers[chars[i]];
-            }
-        }
-        return chars.join('');
-    });
+$(document).ready(readyFunc);
+$(document).ready(function(client , freelancer){
+	$('#btn_sign_up_cl ient').click(function(){
+	client++;
+		window.location.href = "signup-user-pass.html?client" +client ;
+	});
+});
+console.log('Y');
 
 
+
+
+//$('#signup-form').submit(function(){
+//	console.log('kir');
+//	window.location.href = "signup-form.html";
+//});
+var form = $('#signup-form');
+	console.log(form);
+function gotonext(){
+	var validityPass = checkPassword(password.val());
+	var validityUser = checkUserName(username.val());
+	if(validityPass === "okpass" && validityUser =="okusername")
+	window.location.href = "signup-form.html?freelancer=" + freelancer;
+	else
+	$('#error-msg').show();
+}
+function gotonext2(){
+	console.log('kir');
+	var checking = checkmobile(mobileNumber.val());
+	if(checking === 'ok' ){
+ window.location.href = "signup-freelancer-skills.html" ; 
+	}
+
+else $("#ErrorMessage").show();	
+
+}
+	
+
+
+
+//validation of characters
+var checkPassword = function(passText){
+	if (passText.length < 8) 
+	{
+		 return "پسورد شما باید حداقل شامل ۸ حرف باشد";	 
+    } 
+	else if (passText.length > 50)
+	{
+         return "پسورد شما باید حداکثر شامل ۵۰ حرف باشد";
+		
+    } 
+	else if (passText.search(/\d/) == -1) {
+ 		return "پسورد شما باید حداقل شامل یک عدد باشد";
+	} 
+	else if (passText.search(/[A-Z]/) == -1) {
+        return "پسورد شما باید حداقل شامل یک حرف انگلیسی بزرگ باشد";
+	 
+    }
+	else if (passText.search(/[!\@\#\$\%\^\&\*\(\)\_\+]/) != -1) {
+         return "پسورد شما دارای نماد های نامعتبر است";	
+    }
+    else return "okpass";
+}
+
+password.keyup(function(){
+	var passStatus = checkPassword(this.value);
+	if (passStatus === "okpass") 
+	{
+	
+		$('#form-control-feedback-pass').text("پسورد شما مناسب می باشد").css('color' , '#3399ff');
+	
+		
+    } 
+	else
+	{
+       
+		$('#form-control-feedback-pass').text(passStatus).css('display' , 'block').css('color' , 'red');
+		
+    }
+});
+username.keyup(function (){
+	var UserNameX = checkUserName(this.value)
+	if(UserNameX != "okusername"){
+		
+	
+		$("#form-control-feedback-username").text(UserNameX).css('display' , 'block').css('color' , 'red').addClass('class' , 'has-danger');
+	}
+	else 
+		$("#form-control-feedback-username").css('display' , 'none');
+});
+var checkUserName = function (userText){
+
+	if (userText.search(/[!\@\#\$\%\^\&\*\(\)\+]/) != -1)
+	         return "نام کاربری شما دارای نماد های نامعتبر است";	
+	else 
+		return "okusername";
+}
+
+var checkmobile = function(signUpForm) {
+	if(signUpForm.search(/\d/) == -1 ){
+		return 'شماره موبایل شامل کاراکتر های نامعتبر است';
+	}
+	else {
+		return 'ok' ;
+}
+}
+mobileNumber.keyup(function(){
+	var mobile = checkmobile(this.value)
+	if(mobile != 'ok'){
+		$('#mobileError').text(mobile).css('display' , 'block').css('color' , 'red');
+	}
+	else {
+		$('#mobileError').css('display' , 'none');
+	}
 });

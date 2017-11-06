128cd381385bd6820667d184019fa26c87056f0c
Amirhosein Shirani <amirhhh.shirani@yahoo.com>
2017-11-06 21:12:26 +0330

    bug fixed

--- src/reducers/profileInfo.js
+++ src/reducers/profileInfo.js
@@ -2,7 +2,7 @@ import * as types from '../actions/actionTypes.js';
 
 export default function profileInfo(state=[] , action){
   switch (action.type) {
-    case 'LOAD_INFO_SUCESS':
+    case 'LOAD_INFO_SUCCESS':
         return action.profileInfo;
     default:
         return state;

4849d4c9f0f0c87231e3f08474e853d36cc75ad2
hamidrt <toliati.hamid@yahoo.com>
2017-11-16 05:01:26 +0330

    project submission selectpicker final

--- src/components/projectsubmition/Datas/Languages.js
+++ src/components/projectsubmition/Datas/Languages.js
@@ -1,4 +1,12 @@
-exports.AVAILABLELANGUAGES = [
+import React from 'react';
+exports.AVAILABLETOLANGUAGES = [
+  {name: 'فارسی'},
+  {name: 'انگلیسی'},
+  {name: 'فرانسوی'},
+  {name: 'عربی'},
+  {name: 'اسپانیایی'}
+];
+exports.AVAILABLEFROMLANGUAGES = [
   {name: 'فارسی'},
   {name: 'انگلیسی'},
   {name: 'فرانسوی'},

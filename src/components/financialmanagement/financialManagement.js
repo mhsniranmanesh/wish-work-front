4b08dcc2f7811cee2905bcaa1d7bc1870fdeba30
Mohsen Iranmanesh <mhsn.iranmanesh@gmail.com>
2017-11-30 12:10:57 +0330

    financial management refactored

--- src/components/financialmanagement/financialManagement.js
+++ src/components/financialmanagement/financialManagement.js
@@ -141,45 +141,40 @@ class financialManagement extends React.Component{
 
     return(
       <div>
-      <div className="content-wrapper py-3">
-      <div className="container-fluid">
-        <div className="row">
-            <div className="col-sm-8 d-block mx-auto nav-pills">
+        <div className="content-wrapper py-3">
+          <div className="container-fluid">
+            <div className="row">
+              <div className="col-sm-8 d-block mx-auto nav-pills">
                 <div className="dash-con dash-new-project con-body mb-4">
-                    <h5>مدیریت مالی</h5>
+                  <h5>مدیریت مالی</h5>
+                  <div className="dash-divider"></div>
+                  <div className="chart">
+                    <Line data={lineData} />
                     <div className="dash-divider"></div>
-                    <div className="chart">
-                      <Line data={lineData} />
-                        <div className="dash-divider"></div>
-                        <div className="row text-center">
-                            <div className="col-sm-6">
-                                <p>بیشترین مبلغ: <span>۲/۵۰۰/۰۰۰ تومان</span></p>
-                                <p>کمترین مبلغ: <span>۲/۵۰۰/۰۰۰ تومان</span></p>
-                            </div>
-                            <div className="col-sm-6">
-                                <p>مبلغ میانگین: <span>۲/۵۰۰/۰۰۰ تومان</span></p>
-                            </div>
-                        </div>
+                    <div className="row text-center">
+                      <div className="col-sm-6">
+                        <p>بیشترین مبلغ: <span>۲/۵۰۰/۰۰۰ تومان</span></p>
+                        <p>کمترین مبلغ: <span>۲/۵۰۰/۰۰۰ تومان</span></p>
+                      </div>
+                      <div className="col-sm-6">
+                        <p>مبلغ میانگین: <span>۲/۵۰۰/۰۰۰ تومان</span></p>
+                      </div>
                     </div>
+                  </div>
                 </div>
                 <div className="dash-con dash-new-project con-body mb-4">
-
-                /* <Doughnut data={mockData2} options={mockOptions2} width="400" height="200"/>
-                   <Doughnut data={data} /> */
-
-
-
+                  {/*<Doughnut data={mockData2} options={mockOptions2} width="400" height="200"/>*/}
+                  {/*<Doughnut data={data} />*/}
                 </div>
                 <div className="dash-con dash-new-project con-body mb-4">
-                    <div className="chart">
-                        <canvas id="timeColorChart" width="400" height="200"></canvas>
-                    </div>
+                  {/*<div className="chart">*/}
+                    {/*<canvas id="timeColorChart" width="400" height="200"></canvas>*/}
+                  {/*</div>*/}
                 </div>
+              </div>
             </div>
+          </div>
         </div>
-    </div>
-</div>
-
       </div>
     )
   }

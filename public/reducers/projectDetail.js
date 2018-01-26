f50307f8791416ecbde196deab57d5bde58ef35a
Amirhosein Shirani <amirhhh.shirani@yahoo.com>
2018-01-26 16:55:05 +0330

    delete bid

--- public/reducers/projectDetail.js
+++ public/reducers/projectDetail.js
@@ -22,12 +22,13 @@ export default function projectDetail(state= initialState.projectDetail  , actio
 
             };
             var t = size(state);
-            for(var i = 0 ; i < state[0].general.project_bids.length ; i ++){
-                project_bids.project_bids[i] = state[0].general.project_bids[i]
+            for(var i = 0 ; i < state[t-1].general.project_bids.length ; i ++){
+                project_bids.project_bids[i] = state[t-1].general.project_bids[i]
             }
             project_bids.project_bids[i] = action.newAddedBid.data;
-            project_bids.project_bids.length = (state[0].general.project_bids.length + 1);
+            project_bids.project_bids.length = (state[t-1].general.project_bids.length + 1);
             action.newAddedBid.data.uuid = action.newAddedBid.data.id;
+            project_bids.project_bids[i].uuid = action.newAddedBid.data.id;
             project_bids.project_bids[i].freelancer.first_name = action.newAddedBid.first_name;
             project_bids.project_bids[i].freelancer.last_name = action.newAddedBid.last_name;
             // general.project_bids = project_bids ;
@@ -38,13 +39,28 @@ export default function projectDetail(state= initialState.projectDetail  , actio
         case types.SELECT_BID_SUCCESS:
             return state;
         case types.DELETE_BID_SUCCESS:
+            var project_bids2 = {
+                project_bids: [],
+
+            };
             var t2 = size(state);
-            var index = state[t2-1].general.project_bids.indexOf(action.Success.data.bid_id);
+            for(var y = 0 ; y < state[t2-1].general.project_bids.length ; y ++){
+                project_bids2.project_bids[y] = state[t2-1].general.project_bids[y]
+            }
+            var obj = Object.values(state[t2-1].general.project_bids);
+            console.log('TYPE', typeof state[t2-1].general.project_bids);
+            console.log('state[t2-1].general.project_bids' , state[t2-1].general.project_bids);
+            console.log(obj , 'obj');
+            var z = Array.find(obj , (element) => element.uuid === action.Success.data.bid_id);
+            console.log(z , 'z');
+            var index = obj.findIndex(x => x.uuid === z);
+            console.log('index' , index);
+            console.log(project_bids2 , 'project_bids2');
+            project_bids2.project_bids.splice(index , 1);
             debugger;
+            console.log(index, 'index');
                 return [...state, Object.assign({}, state[t2 - 1], {
-                    general: Object.assign({}, state[t2 - 1].general, {
-                        project_bids: Object.assign({}, state[t2 - 1], general.project_bids.splice(index, 1))
-                    })
+                    general: Object.assign({}, state[t2 - 1].general, project_bids2)
                 })];
         default :
             return state;

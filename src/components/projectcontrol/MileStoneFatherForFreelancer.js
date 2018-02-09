af27e180d4f979834db212e5117b3a4ec0636697
Amirhosein Shirani <amirhhh.shirani@yahoo.com>
2018-02-09 15:58:42 +0330

    sort complete for cp

--- src/components/projectcontrol/MileStoneFatherForFreelancer.js
+++ src/components/projectcontrol/MileStoneFatherForFreelancer.js
@@ -7,12 +7,14 @@ const MileStoneFatherForFreelancer = (props) =>{
     var m = [],sortM=[];
     var uploadSendToServerButton = true;
     console.log(props.project_controller.project_controller_elements , 'props.project_controller.project_controller_elements');
-    if(props.project_controller.project_controller_elements.length) {
-        for (var i = 0; i < props.project_controller.project_controller_elements.length; i++) {
-            if(props.mileStoneid === props.project_controller.project_controller_elements[i].uuid ){
+    sortM = arraySort(props.project_controller.project_controller_elements , 'order' );
+
+    if(sortM.length) {
+        for (var i = 0; i < sortM.length; i++) {
+            if(props.mileStoneid === sortM[i].uuid ){
                 uploadSendToServerButton = true;
                 m.push(<MileStoneTemplate key={i}
-                                          CP={props.project_controller.project_controller_elements[i]}
+                                          CP={sortM[i]}
                                           uploadFile={props.uploadFile}
                                           uploadSendToServerButton={uploadSendToServerButton}
                                           sendUploadedFileByFreelancer={props.sendUploadedFileByFreelancer}
@@ -20,7 +22,7 @@ const MileStoneFatherForFreelancer = (props) =>{
             }
             else {
                 m.push(<MileStoneTemplate key={i}
-                                          CP={props.project_controller.project_controller_elements[i]}
+                                          CP={sortM[i]}
                                           uploadFile={props.uploadFile}
                                           uploadSendToServerButton={false}
                 />)
@@ -28,13 +30,12 @@ const MileStoneFatherForFreelancer = (props) =>{
         }
         if (i > 0) {
             console.log(m , 'm');
-            sortM = arraySort(m , 'order');
             return (
                 <div>
                 <div className="container">
                     <div className="row">
                         <div className="timeline-centered">
-                            {sortM}
+                            {m}
                         </div>
                     </div>
                 </div>

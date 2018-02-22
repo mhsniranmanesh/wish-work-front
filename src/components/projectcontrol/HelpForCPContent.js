ace83b3c7b4a7453ca332a6b38f5d7087819dd0e
Amirhosein Shirani <amirhhh.shirani@yahoo.com>
2018-02-22 20:34:20 +0330

    content revise for help cp

--- src/components/projectcontrol/HelpForCPContent.js
+++ src/components/projectcontrol/HelpForCPContent.js
@@ -32,30 +32,34 @@ const HelpForCPContent = (props) =>{
 <p>زرد : در انتظار بازنگری و تایید</p>
 <p>سبز : پایان یافته</p>
 <p>توسی : هنوز نرسیده</p>
+                                        هنگامی که پروژه در حال انجام است برای فریلنسران قابلیت آپلود فایل وجود دارد
+                                        هنگامی که پروژه در مرحله ی بازنگری و تایید است کارفرما ۲۴ ساعت فرصت دارد تا پروژه را به تایید خود برساند و مبلغ فاز بعدی را بپردازد تا فاز بعدی شروع شود
                                     </div>
                                 </div>
                                 <div  className={"tab-pane"+ props.mohlateErsal} id="strategy">
                                     <div className="design-process-content">
                                         <h3 className="semi-bold">مهلت ارسال پروژه توسط فریلنسر</h3>
-                                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincid unt ut laoreet dolore magna aliquam erat volutpat</p>
+                                        <p>در مرحله ی «انجام پروژه» ، فریلنسر مهلت پایانی زمان آپلود فایل خود را مشاهده می نماید، در صورت تاخیر بعد از زمان گفته شده ، پروژه به صورت خودکار لغو می گردد و وجه به حساب کارفرما باز خواهد گشت</p>
                                     </div>
                                 </div>
                                 <div  className={"tab-pane"+ props.mohlateBazNegari} id="optimization">
                                     <div className="design-process-content">
                                         <h3 className="semi-bold">مهلت بازنگری و یا تایید برای کارفرما</h3>
-                                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincid unt ut laoreet dolore magna aliquam erat volutpat</p>
+                                        <p>                                        هنگامی که پروژه در مرحله ی بازنگری و تایید است کارفرما ۲۴ ساعت فرصت دارد تا پروژه را به تایید خود برساند و مبلغ فاز بعدی را بپردازد تا فاز بعدی شروع شود، در صورت تاخیر در تایید و پرداخت فاز بعدی پروژه ، پروژه به صورت خودکار لغو شده و وجه به صورت کامل طی ۴۸ ساعت به حساب فریلنسر پرداخت می گردد
+                                        </p>
                                     </div>
                                 </div>
                                 <div  className={"tab-pane"+ props.laghv} id="content">
                                     <div className="design-process-content">
                                         <h3 className="semi-bold">لغو پروژه</h3>
-                                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincid unt ut laoreet dolore magna aliquam erat volutpat</p>
+                                        <p>در صورت لغو پروژه توسط کارفرما، در صورتی که قبل از دریافت فایل(ها)ی خود باشد، وجه به صورت کامل طی ۴۸ ساعت به حسابش منتقل می شود، همچنین اگر فایل مورد نظر فاقد کیفیت مورد نظر بوده و نیز به تایید ویش ورک برسد وجه به صورت کامل به حساب کارفرما واریز می شود، در غیر این صورت ، وجه بازه های انجام شده به صورت کامل به حساب فریلنسر پرداخته می شود</p>
                                     </div>
                                 </div>
                                 <div  className={"tab-pane"+ props.bishtar} id="reporting">
                                     <div className="design-process-content">
                                         <h3>بیشتر</h3>
-                                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincid unt ut laoreet dolore magna aliquam erat volutpat. </p>
+                                        <p>ویش ورک هیچ گونه مبلغی را از مبلغ پرداختی توسط کارفرما کاهش نمی دهد و صرفا واسطه می باشد، برای اطلاعات بیشتر با شماره ی زیر تماس حاصل فرمایید:</p>
+                                        <p>۰۲۱-۸۸۴۶۶۹۵۹</p>
                                     </div>
                                 </div>
                             </div>
